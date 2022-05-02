33791e40acfdf5b346863a5b0aace6d71fdf4877
barisakr <baris.akr@gmail.com>
2022-05-02 20:36:19 +0300

    EntityFramework Applied

--- Api/Startup.cs
+++ Api/Startup.cs
@@ -1,20 +1,24 @@
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 
 namespace API
 {
     public class Startup
     {
+        private readonly IConfiguration _configuration;
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            _configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
+
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<StoreContext>(x => x.UseSqlite(
+                _configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
